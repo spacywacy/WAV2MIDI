d1beb7d9a703b8f94678115432efa07af0d7f950
Larry Zhou <larryzms@gmail.com>
2019-04-02 15:31:15 -0400

    add testing module

---
+++ test.py
@@ -0,0 +1,143 @@
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from random import random
+import matplotlib.pyplot as plt
+import nets
+import pickle
+from time import time
+
+
+class Test():
+
+	def __init__(self, data_dir, model, loss=None, n_batch=None):
+		#io
+		self.data_dir = data_dir
+		self.model_bin = 'model_bin'
+
+		#net
+		if type(model) == str:
+			model_full_dir = os.path.join(self.model_bin, model)
+			self.net = self.load_model(model_full_dir)
+		else:
+			self.net = model
+
+		#loss
+		if loss:
+			self.criterion = loss
+		else:
+			self.criterion = nn.MSELoss()
+
+		#utilities
+		self.label_len = 88
+		self.n_batch = n_batch
+
+		#results
+		self.batch_losses = []
+		self.loss = 0.0
+
+
+	def load_model(self, fname):
+		with open(fname, 'rb') as f:
+			obj = pickle.load(f)
+		return obj
+
+	def run_test(self):
+		if self.n_batch:
+			self.test_w_batch()
+		else:
+			self.test_all_data()
+
+		return self.loss
+
+	def test_all_data(self):
+		with open(self.data_dir, 'r') as f:
+			all_data = []
+			for line in f:
+				data_row = [float(x) for x in line[:-1].split(',')]
+				all_data.append(data_row)
+
+			Xs = torch.tensor([x[:-self.label_len] for x in all_data], dtype=torch.float)
+			y = torch.tensor([x[-self.label_len:] for x in all_data], dtype=torch.float)
+			output = self.net(Xs)
+			self.loss = self.criterion(output, y)
+			print('{} on all data: {}'.format(str(self.criterion), str(self.loss)))
+
+	def test_w_batch(self):
+		i_batch = 0
+		for batch in self.load_batch():
+			if i_batch >= self.n_batch:
+				break
+
+			Xs = torch.tensor([x[:-self.label_len] for x in batch], dtype=torch.float)
+			y = torch.tensor([x[-self.label_len:] for x in batch], dtype=torch.float)
+			output = self.net(Xs)
+			self.batch_losses.append(self.criterion(output, y))
+			i_batch += 1
+
+		self.loss = sum(self.batch_losses)/len(self.batch_losses)
+		print('Average {} over {} batches: {}'.format(str(self.criterion), str(self.n_batch), str(self.loss)))
+
+	def load_batch(self):
+		i_row = 1
+		batch = []
+
+		with open(self.data_dir, 'r') as f:
+			for line in f:
+				if i_row % self.batch_size == 0 and i_row!=1:
+					yield batch
+					batch = []
+
+				try:
+					data_row = [float(x) for x in line[:-1].split(',')]
+				except Exception as e:
+					print('Error when appending row({}): {}'.format(str(i_row), str(e)))
+
+				batch.append(data_row)
+				i_row += 1
+
+
+
+def test_Test():
+	data_dir = 'data/dataset_validation.csv'
+	model = 'simply_fc_1554228850.pickle'
+	test_obj = Test(data_dir, model)
+	test_obj.run_test()
+
+
+if __name__ == '__main__':
+	test_Test()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import torch
 import torch.nn as nn
@@ -6,70 +7,119 @@ import torch.optim as optim
 from random import random
 import matplotlib.pyplot as plt
 import nets
+import pickle
+from time import time
 
 
-def read_batch(data_dir, batch_size):
-	i_row = 1
-	batch = []
+class Train():
 
-	with open(data_dir, 'r') as f:
-		for line in f:
-			if i_row%batch_size == 0 and i_row!=1:
-				clean_batch = True
-				yield batch
-				batch = []
+	def __init__(self):
+		#io
+		self.data_dir = 'data/dataset_train.csv'
+		self.model_bin = 'model_bin'
+		if not os.path.exists(self.model_bin):
+			os.makedirs(self.model_bin)
 
-			try:
-				data_row = [float(x) for x in line[:-1].split(',')]
-			except Exception as e:
-				print('Error when appending row({}): {}'.format(str(i_row), str(e)))
+		#net & loss
+		self.net = nets.FC_NET()
+		self.criterion = nn.MSELoss()
+		self.learning_rate = 0.000000001
+		self.optimizer = optim.SGD(self.net.parameters(), lr=self.learning_rate)
 
-			batch.append(data_row)
-			i_row+=1
-
-def single_epoch(i_epoch=0):
-	i_batch = 0
-	for batch in read_batch(data_dir, batch_size):
-		if i_batch >= n_batch:
-			break
-
-		Xs = torch.tensor([x[:-label_len] for x in batch], dtype=torch.float)
-		y = torch.tensor([x[-label_len:] for x in batch], dtype=torch.float)
+		#training specifics
+		self.batch_size = 100 #size of a single batch
+		self.n_batch = 2 #number of batches to load from dataset
+		self.n_epoch = 2 #number of epochs
 		
-		optimizer.zero_grad()
-		output = net(Xs)
-		loss = criterion(output, y)
-		cost_history.append(loss)
-		print('epoch: {}, batch: {}, loss: {}'.format(str(i_epoch), str(i_batch), str(float(loss.data))))
-		loss.backward()
-		optimizer.step()
-		i_batch+=1
+		#utilities
+		self.label_len = 88 #length of the label vector
+		self.cost_history = [] #losses over batches
+		self.cost_plot_offset = 5 #ignore cost first n number of batch during plotting
 
-def train_loop():
-	for i_epoch in range(n_epoch):
-		single_epoch(i_epoch)
+	def load_batch(self):
+		i_row = 1
+		batch = []
 
-def plot_cost(cost_history):
-	xs = list(range(5, len(cost_history)))
-	plt.plot(xs, cost_history[5:])
-	plt.xlabel('Iterations')
-	plt.ylabel('Cost')
-	plt.show()
+		with open(self.data_dir, 'r') as f:
+			for line in f:
+				if i_row % self.batch_size == 0 and i_row!=1:
+					yield batch
+					batch = []
+
+				try:
+					data_row = [float(x) for x in line[:-1].split(',')]
+				except Exception as e:
+					print('Error when appending row({}): {}'.format(str(i_row), str(e)))
+
+				batch.append(data_row)
+				i_row += 1
+
+	def single_epoch(self, i_epoch=0):
+		i_batch = 0
+		for batch in self.load_batch():
+			if i_batch >= self.n_batch:
+				break
+
+			Xs = torch.tensor([x[:-self.label_len] for x in batch], dtype=torch.float)
+			y = torch.tensor([x[-self.label_len:] for x in batch], dtype=torch.float)
+			
+			self.optimizer.zero_grad()
+			output = self.net(Xs)
+			loss = self.criterion(output, y)
+			self.cost_history.append(loss)
+			print('epoch: {}, batch: {}, loss: {}'.format(str(i_epoch), str(i_batch), str(float(loss.data))))
+			loss.backward()
+			self.optimizer.step()
+			i_batch += 1
+
+	def train_loop(self):
+		for i_epoch in range(self.n_epoch):
+			self.single_epoch(i_epoch)
+		print('Done training')
+
+		self.close_train()
+		print('Dumped files')
+
+	def plot_cost(self, plt_dir=None):
+		xs = list(range(5, len(self.cost_history)))
+		plt.plot(xs, self.cost_history[self.cost_plot_offset:])
+		plt.xlabel('Iterations')
+		plt.ylabel('Cost')
+		if plt_dir:
+			plt.savefig(plt_dir)
+		else:
+			plt.show()
+
+	def pickle_dump(self, fname, obj_):
+		with open(fname, 'wb') as f:
+			pickle.dump(obj_, f)
+
+	def pickle_load(self, fname):
+		with open(fname, 'rb') as f:
+			obj = pickle.load(f)
+		return obj
+
+	def write_model_desc(self, fname):
+		with open(fname, 'w') as f:
+			f.write('Net name: {}\n'.format(self.net.name))
+			f.write('Loss: {}\n'.format(str(self.criterion)))
+			f.write('Learning rate: {}\n'.format(self.learning_rate))
+			f.write('Total batches: {}\n'.format(self.n_batch))
+			f.write('Total epochs: {}\n'.format(self.n_epoch))
+
+	def close_train(self):
+		model_name = '{}_{}'.format(self.net.name, str(int(time())))
+		model_pickle_dir = os.path.join(self.model_bin, model_name + '.pickle')
+		model_desc_dir = os.path.join(self.model_bin, model_name + '.txt')
+		model_graph_dir = os.path.join(self.model_bin, model_name + '.png')
+		self.pickle_dump(model_pickle_dir, self.net)
+		self.plot_cost(plt_dir=model_graph_dir)
+		self.write_model_desc(model_desc_dir)
+
 
 
 if __name__ == '__main__':
-
-	data_dir = 'data/dataset_train.csv'
-	batch_size = 1000
-	n_batch = 50
-	n_epoch = 10
-	label_len = 88
-	net = nets.FC_NET()
-	criterion = nn.MSELoss()
-	optimizer = optim.SGD(net.parameters(), lr=0.000000001)
-	cost_history = []
-	train_loop()
-	plot_cost(cost_history)
+	Train().train_loop()
 
 
 
